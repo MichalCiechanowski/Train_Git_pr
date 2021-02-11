af4765bf019cc2f317cc785ac98aeb47461fa299
MichalCiechanowski <45978311+MichalCiechanowski@users.noreply.github.com>
2021-02-11 16:00:02 +0100

    02 Zmiana na mnożenie w metodzie policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz(int a.int b){
+return a*b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W pierwszym branchu; 2+3=" << Policz(a,b) << std::endl;
     return 0;
 }
